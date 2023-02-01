4cc914183cf8e1b7e8adb1c79e7da5df286b80d9
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-01 23:23:21 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (65.71%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -5,25 +5,10 @@ public:
             swap(str2,str1);
         int m=str1.size();
         int n=str2.size();
-        for(int k=m;k>0;k--)
-        {
-            if(n%k!=0)continue;
-            if(m%k!=0)continue;
-            string ans=str1.substr(0,k);
-            string test=ans;
-            int a=m/k;
-            for(int i=1;i<a;i++)
-                test+=ans;
-            if(str1!=test)
-                continue;
-            test=ans;
-            a=n/k;
-            for(int i=1;i<a;i++)
-                test+=ans;
-            if(str2!=test)
-                continue;
-            return ans;
-        }
-        return "";
+        if(str1+str2!=str2+str1)
+            return "";
+        int k=__gcd(m,n);
+        string g=str1.substr(0,k);
+        return g;
     }
 };
