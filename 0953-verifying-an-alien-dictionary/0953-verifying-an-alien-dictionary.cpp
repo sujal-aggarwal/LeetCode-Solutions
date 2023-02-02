cec12fef3fd406827e88a402167df1898f291ba1
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-02 15:25:13 +0530

    Time: 8 ms (43.80%), Space: 9.9 MB (10.75%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    bool isSorted(vector<string>& words,string order,int index)
+    bool isSorted(vector<string>& words,string order,int index,int*dp)
     {
         if(index==words.size()||index==words.size()-1)
-            return true;
-        bool ans=isSorted(words,order,index+1);
+            return dp[index]=true;
+        if(dp[index]!=-1)
+            return dp[index];
+        bool ans=isSorted(words,order,index+1,dp);
         if(!ans)
-            return false;
+            return dp[index]=false;
         for(int i=0;i<min(words[index].size(),words[index+1].size());i++)
         {
             int a=find(order.begin(),order.end(),words[index][i])-order.begin();
             int b=find(order.begin(),order.end(),words[index+1][i])-order.begin();
             if(a>b)
-                return false;
+                return dp[index]=false;
             else if(a<b)
-                return true;
+                return dp[index]=true;
         }
         if(words[index].size()>words[index+1].size())
-            return false;
-        return true;
+            return dp[index]=false;
+        return dp[index]=true;
     }
     bool isAlienSorted(vector<string>& words, string order) {
-        return isSorted(words,order,0);
+        int *dp=new int[words.size()+1];
+        for(int i=0;i<=words.size();i++)
+            dp[i]=-1;
+        return isSorted(words,order,0,dp);
     }
 };
