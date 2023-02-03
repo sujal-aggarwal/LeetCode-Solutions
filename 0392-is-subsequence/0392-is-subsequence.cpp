6913dfaf8999593bdff131ad4bc5a78133ba1dae
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-03 19:04:19 +0530

    Time: 39 ms (5.02%), Space: 140.2 MB (5.01%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.size()==0)
+            return true;
+        else if(t.size()==0)
+            return false;
+        if(s[0]==t[0])
+            return isSubsequence(s.substr(1),t.substr(1));
+        return isSubsequence(s,t.substr(1));
+    }
+};
