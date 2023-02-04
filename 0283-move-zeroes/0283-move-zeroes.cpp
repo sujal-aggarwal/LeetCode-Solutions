5c41f6c7ab9cc2db22091fa96fa5ed4329047e09
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-05 00:38:21 +0530

    Time: 24 ms (75.69%), Space: 19.3 MB (64.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -6,10 +6,8 @@ public:
         {
             if(nums[cur]!=0)
             {
-                nums[i++]=nums[cur];
+                swap(nums[i++],nums[cur]);
             }
         }
-        for(;i<nums.size();i++)
-            nums[i]=0;
     }
 };
