39482f2f0cf77f0033dafac5640182a596257afd
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-07 23:55:06 +0530

    Time: 177 ms (76.97%), Space: 75.3 MB (13.09%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -7,19 +7,17 @@ public:
         int maxsize=1;
         map<int,int>mp;
         mp[fruits[i]]++;
-        s.insert(fruits[i]);
         while(j<fruits.size())
         {
-            s.insert(fruits[j]);
             mp[fruits[j]]++;
-            if(s.size()>2)
+            if(mp.size()>2)
             {
                 maxsize=max(maxsize,j-i);
-                while(s.size()!=2)
+                while(mp.size()!=2)
                 {
                     mp[fruits[i]]--;
                     if(mp[fruits[i]]==0)
-                        s.erase(fruits[i]);
+                        mp.erase(fruits[i]);
                     i++;
                 }
             }
@@ -28,4 +26,33 @@ public:
         maxsize=max(maxsize,j-i);
         return maxsize;
     }
+//     class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         // We use a hash map 'basket' to store the number of each type of fruit.
+//         unordered_map<int, int> basket;
+//         int left = 0, maxPicked = 0;
+        
+//         // Add fruit from the right index (right) of the window.
+//         for (int right = 0; right < fruits.size(); ++right) {
+//             basket[fruits[right]]++;
+            
+//             // If the current window has more than 2 types of fruit,
+//             // we remove fruit from the left index (left) of the window,
+//             // until the window has only 2 types of fruit.
+//             while (basket.size() > 2) {
+//                 basket[fruits[left]]--;
+//                 if (basket[fruits[left]] == 0)
+//                     basket.erase(fruits[left]);
+//                 left++;
+//             }
+            
+//             // Update maxPicked.
+//             maxPicked = max(maxPicked, right - left + 1);
+//         }
+        
+//         // Return maxPicked as the maximum number of fruits we can collect.
+//         return maxPicked;
+//     }
+// };
 };
