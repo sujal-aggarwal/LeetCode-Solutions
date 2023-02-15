b1fa190275c5157b9147f099e55f16f20a3c9d33
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-02-15 18:06:54 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.26%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t a=0;
+        for(int i=0;i<32;i++)
+        {
+            a<<=1;
+            a+=n%2;
+            n>>=1;
+        }
+        return a;
+    }
+};
