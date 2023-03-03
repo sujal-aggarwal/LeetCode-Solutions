c10ef79f3926af6d652db5e37275583544b7d5d2
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-03-03 19:44:38 +0530

    Time: 45 ms (85.74%), Space: 31.4 MB (55.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        /*
-            This question uses a new approach 
-            i.e. Negative Marking
-        */
-        for(int j=0;j<nums.size();j++){
-            while(nums[j]>0&&nums[j]<=nums.size()&&nums[nums[j]-1]!=nums[j]){
-                swap(nums[nums[j]-1],nums[j]);
+        int n = nums.size();
+        for(auto &i: nums){
+            if(i<=0 || i>n){
+                i = n+1;
             }
         }
-        for(auto i:nums){
-            cout<<i<<" ";
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=i+1){
-                return i+1;
+        
+        for(auto &i: nums){
+            if(abs(i)<=n){
+                if(nums[abs(i)-1]>0)
+                nums[abs(i)-1] = -nums[abs(i)-1];
             }
         }
-        return nums.size()+1;
+        for(int i = 0; i < n; i++){
+            if(nums[i]>=0)return i+1;
+        }
+        return n+1;
     }
 };
