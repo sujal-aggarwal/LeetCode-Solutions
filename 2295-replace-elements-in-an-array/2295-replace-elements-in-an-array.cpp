3071c3caa2b30fa3b98844c84a5c8274e5b56d73
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-03-03 22:01:40 +0530

    Time: 551 ms (42.98%), Space: 137 MB (38.42%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -1,18 +1,17 @@
+unordered_map<int,int>m;
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
         int n=nums.size();
-        unordered_map<int,int>index;
         for(int i=0;i<n;i++){
-            index[nums[i]]=i;
+            m[nums[i]]=i;
         }
         for(auto i:operations){
             int element=i[0];
             int new_element=i[1];
-            int j=index[element];
+            int j=m[element];
             nums[j]=new_element;
-            index.erase(element);
-            index[new_element]=j;
+            m[new_element]=j;
         }
         return nums;
     }
