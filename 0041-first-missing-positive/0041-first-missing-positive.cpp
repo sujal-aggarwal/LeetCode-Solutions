4a3a1b2b820f1e82bfd38f7b81fc785bf08af63c
codeblaster8109 <114332825+codeblaster8109@users.noreply.github.com>
2023-03-03 20:16:46 +0530

    Time: 46 ms (84.06%), Space: 31.4 MB (85.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        
+        //Here first we aremaking every <=0 and >n element equal to n+1 cuz it will be the answer none the less
         int n = nums.size();
         for(auto &i: nums){
             if(i<=0 || i>n){
@@ -8,6 +10,8 @@ public:
             }
         }
         
+        
+        //Now here we are using Negative Marking concept as we are taking each element and going to its respective index and marking -1 there by doing this we can simply check for the missing element as the only index where we wouldn't have visited
         for(auto &i: nums){
             if(abs(i)<=n){
                 if(nums[abs(i)-1]>0)
@@ -17,6 +21,7 @@ public:
         for(int i = 0; i < n; i++){
             if(nums[i]>=0)return i+1;
         }
+        
         return n+1;
     }
 };
