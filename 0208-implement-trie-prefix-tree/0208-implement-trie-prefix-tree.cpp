13d4fad0430bc5e78894c880d92e015a10341989
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-03-19 20:19:38 +0530

    Time: 99 ms (20.64%), Space: 43.2 MB (86.18%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,48 +1,46 @@
 class TrieNode{
-public:
-    bool isCompleteWord;
-    TrieNode* children[26];
-
-    TrieNode() {
-        isCompleteWord = false; // when the word is complete (mark that node as true)
-        memset(children, 0, sizeof(children)); // for 26 possible Childrens (for next letter)
+    public:
+    unordered_map<char,TrieNode*>children;
+    bool isEnd;
+    TrieNode(){
+        isEnd=false;
     }
 };
-
 class Trie {
+private:
+    TrieNode*root;
 public:
-    TrieNode* root;
-
     Trie() {
-        root = new TrieNode();
+        root=new TrieNode();
     }
     
     void insert(string word) {
-        TrieNode* node = root;
-        for (char ch : word){
-            int idx = ch - 'a';
-            if(node->children[idx]==NULL) node->children[idx] = new TrieNode();
-            node = node->children[idx];
+        TrieNode*node=root;
+        for(auto i:word){
+            if(node->children.find(i)==node->children.end()){
+                node->children[i]=new TrieNode();
+            }
+            node=node->children[i];
         }
-        node->isCompleteWord = true;
+        node->isEnd=true;
     }
     
     bool search(string word) {
-        TrieNode* node = root;
-        for(char ch : word){
-            int idx = ch - 'a';
-            if(node->children[idx]==NULL) return false;
-            node = node->children[idx];
+        TrieNode*node=root;
+        for(auto i:word){
+            if(node->children.find(i)==node->children.end())
+                return false;
+            node=node->children[i];
         }
-        return node->isCompleteWord;
+        return node->isEnd;
     }
     
-    bool startsWith(string prefix) {
-        TrieNode* node = root;
-        for(char ch : prefix){
-            int idx = ch - 'a';
-            if(node->children[idx]==NULL) return false;
-            node = node->children[idx];
+    bool startsWith(string word) {
+        TrieNode*node=root;
+        for(auto i:word){
+            if(node->children.find(i)==node->children.end())
+                return false;
+            node=node->children[i];
         }
         return true;
     }
