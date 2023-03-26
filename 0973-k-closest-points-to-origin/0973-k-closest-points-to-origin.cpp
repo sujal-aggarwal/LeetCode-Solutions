e6d1903749c08652c975e23fbda7bf3db0a7138c
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-03-26 18:11:01 +0530

    Time: 171 ms (98.18%), Space: 51.7 MB (99.80%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>>ans(k);
-        priority_queue<vector<int>>max_heap;
-        for(auto &i:points){
-            int x=i[0],y=i[1];
-            max_heap.push({x*x+y*y,x,y});
-            if(max_heap.size()>k){
-                max_heap.pop();
-            }
-        }
-        for(int i=0;i<k;i++){
-            vector<int>arr=max_heap.top();
-            max_heap.pop();
-            ans[i]={arr[1],arr[2]};
-        }
-        return ans;
+        nth_element(begin(points), begin(points) + k, end(points), [](const auto& a, const auto& b) {
+            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
+        });
+        points.resize(k);
+        return move(points);
     }
 };
