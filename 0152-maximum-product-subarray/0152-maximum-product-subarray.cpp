be4f66fa4295a9777da2bab40b6373ad9fdf310e
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-04-01 17:11:35 +0530

    Time: 7 ms (73.96%), Space: 13.9 MB (9.55%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& arr) {
-        int max_ending_here=1;
-        int min_ending_here=1;
-        int max_val=INT_MIN;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<0){
-                int temp=max_ending_here;
-                max_ending_here=max(INT_MIN,min_ending_here*arr[i]);
-                min_ending_here=min(1,temp*arr[i]);
-                max_val=max(max_ending_here,max_val);
-                if(max_ending_here<1){
-                    max_ending_here=1;
-                }
-            }else if(arr[i]>0){
-                max_ending_here*=arr[i];
-                min_ending_here=min(1,min_ending_here*arr[i]);
-                max_val=max(max_ending_here,max_val);
-            }else{
-                min_ending_here=1;
-                max_ending_here=1;
-                max_val=max(0,max_val);
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int minProduct = 1;
+        int maxProduct = 1;
+        int ans = INT_MIN;
+        for(int i=0; i<n; i++){
+            if(nums[i] < 0){
+                swap(maxProduct, minProduct);
             }
+            maxProduct = max(maxProduct*nums[i], nums[i]);
+            minProduct = min(minProduct*nums[i], nums[i]);
+            ans = max(ans, maxProduct);
         }
-        return max_val;
+        return ans;
     }
 };
