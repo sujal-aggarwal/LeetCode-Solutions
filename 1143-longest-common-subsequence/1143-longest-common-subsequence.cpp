3c340cc55aa6c0745e849a804afb6d076f7e2b0a
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-04-16 14:45:42 +0530

    Time: 22 ms (95.63%), Space: 10.6 MB (87.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,7 +3,8 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size();
         int n=text2.size();
-        vector dp(m+1,vector<int>(n+1));
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(text1[i]==text2[j]){
