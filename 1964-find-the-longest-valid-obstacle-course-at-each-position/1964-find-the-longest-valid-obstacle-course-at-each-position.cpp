d459da48c26f2eb943ed87e1e2f5a57d0956f13c
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-05-08 00:38:49 +0530

    Time: 394 ms (26.39%), Space: 123.1 MB (47.22%) - LeetHub

---
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        vector<int>arr;
+        vector<int>ans;
+        for(auto i:obstacles){
+            int index=upper_bound(arr.begin(),arr.end(),i)-arr.begin();
+            if(index==arr.size()){
+                ans.push_back(arr.size()+1);
+                arr.push_back(i);
+            }else{
+                ans.push_back(index+1);
+                arr[index]=i;
+            }
+        }
+        return ans;
+    }
+};
