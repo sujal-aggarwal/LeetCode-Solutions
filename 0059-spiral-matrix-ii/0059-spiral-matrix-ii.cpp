d854f0364ce3a6d9e4edef92cf2ddfabfe221bba
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-05-10 11:46:34 +0530

    Time: 4 ms (33.05%), Space: 6.7 MB (23.20%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector ans(n,vector<int>(n));
+        int row_end=n-1,col_end=n-1,col_start=0,row_start=0;
+        int cnt=1;
+        while(row_end>=row_start&&col_end>=col_start){
+            for(int i=col_start;i<=col_end;i++){
+                ans[row_start][i]=cnt++;
+            }
+            row_start++;
+            if(row_end>=row_start){
+                for(int i=row_start;i<=row_end;i++){
+                    ans[i][col_end]=cnt++;
+                }
+                col_end--;
+                if(col_end>=col_start){
+                    for(int i=col_end;i>=col_start;i--){
+                        ans[row_end][i]=cnt++;
+                    }
+                    row_end--;
+                    if(row_end>=row_start){
+                        for(int i=row_end;i>=row_start;i--){
+                            ans[i][col_start]=cnt++;
+                        }
+                        col_start++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
