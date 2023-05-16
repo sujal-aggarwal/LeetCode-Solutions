e1a526b7f7ab012a2f86bb581f74e0d789acba37
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-05-16 14:24:41 +0530

    Time: 39 ms (59.82%), Space: 70.7 MB (94.75%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,41 +10,45 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode*l1){
-        if(!l1->next){return l1;}
-        ListNode*prev=l1->next;
-        ListNode *ans=reverseList(l1->next);
-        if(prev)
-            prev->next=l1;
-        l1->next=nullptr;
-        return ans;
-    }
-    ListNode* add(ListNode* l1, ListNode* l2) { 
-        int carry=0;
-        ListNode*ans=new ListNode();
-        ListNode*a=ans;
-        while(l1||l2||carry){
-            ListNode*temp=new ListNode();
-            int a=0;
-            if(l1){
-                a+=l1->val;
-                l1=l1->next;
-            }
-            if(l2){
-                a+=l2->val;
-                l2=l2->next;
-            }
-            a+=carry;
-            temp->val=(a%10);
-            ans->next=temp;
-            ans=temp;
-            carry=a/10;
+    ListNode* add(ListNode* l1, ListNode* l2,int&carry) {
+        if(!l1||!l2){
+            return NULL;
         }
-        return a->next;
+        int val=0;
+        ListNode*ans=add(l1->next,l2->next,val);
+        l1->val+=(l2->val+val);
+        carry=l1->val/10;
+        l1->val%=10;
+        l1->next=ans;
+        return l1;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1=reverseList(l1);
-        l2=reverseList(l2);
-        return reverseList(add(l1,l2));
+        ListNode*p1=l1,*p2=l2;
+        while(p1||p2){
+            if(!p1){
+                ListNode*a=new ListNode(0);
+                a->next=l1;
+                l1=a;
+                p2=p2->next;
+            }
+            else if(!p2){
+                ListNode*a=new ListNode(0);
+                a->next=l2;
+                l2=a;
+                p1=p1->next;
+            }
+            else{
+                p1=p1->next;
+                p2=p2->next;
+            }
+        }
+        int ans=0;
+        ListNode*node=add(l1,l2,ans);
+        if(ans){
+            ListNode*n=new ListNode(ans);
+            n->next=node;
+            return n;
+        }
+        return node;
     }
 };
