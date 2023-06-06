e9fcc1959cc96b5b07029e3109e409c3f2f408ff
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-06-06 15:16:18 +0530

    Time: 4 ms (74.54%), Space: 9.7 MB (5.60%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        int d=arr[1]-arr[0];
-        for(int i=2;i<arr.size();i++){
-            if(d!=arr[i]-arr[i-1]){
-                return false;
+        if(arr.size()==2)return true;
+        unordered_map<int,int>mp;
+        int min_element=INT_MAX;
+        int max_element=INT_MIN;
+        int dflag=0;
+        for(auto i:arr){
+            mp[i]++;
+            if(mp[i]>1){
+                dflag=1;
             }
+            min_element=min(min_element,i);
+            max_element=max(max_element,i);
         }
+        if(max_element==min_element)return true;
+        if(dflag==1)return false;
+        int diff=max_element-min_element;
+        if(diff%(arr.size()-1)!=0)return false;
+        diff/=(arr.size()-1);
+        while(min_element<max_element){
+            if(mp[min_element]<=0)return false;
+            min_element+=diff;
+        }
+        if(min_element!=max_element)return false;
         return true;
     }
 };
