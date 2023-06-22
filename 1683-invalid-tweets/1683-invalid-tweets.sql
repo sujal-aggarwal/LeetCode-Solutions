540ee4294fdb5c81e9d37a8df75f10b66eb11a65
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-06-23 01:20:10 +0530

    Time: 2903 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT tweet_id FROM (SELECT tweet_id,LENGTH(content) AS length FROM tweets) AS s WHERE length>15;
+SELECT tweet_id FROM tweets WHERE LENGTH(content)>15;
