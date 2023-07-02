3009b91cb59d87a2c3794d64b7c1d6aeea6b0799
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-03 00:06:15 +0530

    Time: 1104 ms (15.69%), Space: 252 MB (14.71%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<pair<int,int>>arr;
-    int dp(vector<vector<int>>&requests,int index,int mask){
+    int ans(vector<vector<int>>&requests,int index,int mask,vector<vector<int>>&dp){
         if(index==requests.size()){
             for(auto j:arr){
                 if(j.first!=j.second)return 0;
             }
             return __builtin_popcount(mask);
         }
+        if(dp[index][mask]!=-1)return dp[index][mask];
         auto temp=requests[index];
         int f=temp[0];
         int s=temp[1];
         
         arr[f].second++;
         arr[s].first++;
-        int a=dp(requests,index+1,mask|(1<<index));
+        int a=ans(requests,index+1,mask|(1<<index),dp);
         arr[f].second--;
         arr[s].first--;
         
-        int b=dp(requests,index+1,mask);
+        int b=ans(requests,index+1,mask,dp);
         
-        return max(a,b);
+        return dp[index][mask]=max(a,b);
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
+        int m=requests.size();
         arr.resize(n,{0,0});
-        return dp(requests,0,0);
+        vector dp(m,(vector<int>(1<<m,-1)));
+        return ans(requests,0,0,dp);
     }
 };
