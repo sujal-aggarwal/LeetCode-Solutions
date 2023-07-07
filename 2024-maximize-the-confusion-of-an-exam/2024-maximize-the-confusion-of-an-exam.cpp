2c21c2d6880a8ccaad25fd4a7c3123842ba4b140
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-08 00:44:49 +0530

    Time: 20 ms (94.46%), Space: 9.9 MB (95.61%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -4,24 +4,14 @@ public:
         int n=answerKey.size();
         int i=0,j=0,ans=0;
         int f=0;
-        while(j<n){
-            if(answerKey[j]=='F')f++;
-            if(f>k){
-                while(f>k){
-                    if(answerKey[i]=='F')f--;
-                    i++;
-                }
-            }
-            ans=max(ans,j-i+1);
-            j++;
-        }
-        i=0;j=0;
         int t=0;
         while(j<n){
             if(answerKey[j]=='T')t++;
-            if(t>k){
-                while(t>k){
+            if(answerKey[j]=='F')f++;
+            if(min(t,f)>k){
+                while(min(t,f)>k){
                     if(answerKey[i]=='T')t--;
+                    if(answerKey[i]=='F')f--;
                     i++;
                 }
             }
