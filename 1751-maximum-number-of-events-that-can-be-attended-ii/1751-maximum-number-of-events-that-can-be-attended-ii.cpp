dc1f270cfafd8a9864b99f302514278dcf125e78
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-16 01:34:45 +0530

    Time: 381 ms (21.64%), Space: 113.6 MB (5.05%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>>dp;
+    int ans(vector<vector<int>>&events, int ind,int k){
+        if(ind==n||k==0)return 0;
+        if(dp[ind][k]!=-1)return dp[ind][k];
+        int a=ans(events,ind+1,k);
+        vector<int>temp={events[ind][1],INT_MAX,INT_MAX};
+        int next_ind=upper_bound(events.begin()+ind+1,events.end(),temp)-events.begin();
+        int b=ans(events,next_ind,k-1)+events[ind][2];
+        return dp[ind][k]=max(a,b);
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        n=events.size();
+        sort(events.begin(),events.end());
+        dp.resize(n+1,vector<int>(k+1,-1));
+        return ans(events,0,k);
+    }
+};
