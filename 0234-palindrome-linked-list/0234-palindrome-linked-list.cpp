0284ebb0da1ab6c3489cf2c569541e59c6b306e2
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-27 01:46:40 +0530

    Time: 262 ms (59.39%), Space: 128.2 MB (19.93%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int>arr;
+        while(head!=NULL){
+            arr.push_back(head->val);
+            head=head->next;
+        }
+        int i=0,j=arr.size()-1;
+        while(j>=i){
+            if(arr[i++]!=arr[j--])return false;
+        }
+        return true;
+    }
+};
