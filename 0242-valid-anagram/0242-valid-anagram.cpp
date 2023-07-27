09ff12611a35b65044cbe86ede015d68ed23a34c
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-27 15:45:25 +0530

    Time: 13 ms (54.83%), Space: 7.3 MB (39.19%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(int i=0;i<s.length();i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        return mp1==mp2;
+    }
+};
