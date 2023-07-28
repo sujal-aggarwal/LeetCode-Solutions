699def325239a708b99f4672b31cf70af8c8f4d9
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-29 00:20:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (17.26%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int n;
-    bool winner(vector<int>&nums,int i,int j,int sum1,int sum2,bool turn){
+    vector<vector<int>>dp;
+    int winner(vector<int>&nums,int i,int j){
         if(j<i){
-            return sum1>=sum2;
+            return 0;
         }
-        bool left,right;
-        if(turn){
-            left=winner(nums,i+1,j,sum1+nums[i],sum2,!turn);
-            right=winner(nums,i,j-1,sum1+nums[j],sum2,!turn);
-            return max(left,right);
-        }
-        left=winner(nums,i+1,j,sum1,sum2+nums[i],!turn);
-        right=winner(nums,i,j-1,sum1,sum2+nums[j],!turn);
-        return min(left,right);
+        if(dp[i][j]!=-1)return dp[i][j];
+        int left=nums[i]+min(winner(nums,i+2,j),winner(nums,i+1,j-1));
+        int right=nums[j]+min(winner(nums,i,j-2),winner(nums,i+1,j-1));
+        return dp[i][j]=max(left,right);
     }
     bool PredictTheWinner(vector<int>& nums) {
         n=nums.size();
-        return winner(nums,0,n-1,0,0,true);
+        dp.resize(n,vector(n,-1));
+        int a=winner(nums,0,n-1);
+        return a>=(accumulate(nums.begin(),nums.end(),0)-a);
     }
 };
