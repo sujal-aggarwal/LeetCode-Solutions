b7b8efbfc1847537032b396209e37d7d2a29261a
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-07-29 02:10:16 +0530

    Time: 36 ms (97.66%), Space: 15.4 MB (97.28%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
+    void add_bit(char ch,int&bit){
+        bit=bit<<2;
+        if(ch=='C')bit=bit|1;
+        if(ch=='G')bit=bit|2;
+        if(ch=='T')bit=bit|3;
+    }
+    int bit(string&s,int idx){
+        int bit=0;
+        for(int i=0;i<10;i++){
+            add_bit(s[idx+i],bit);
+        }
+        return bit;
+    }
     vector<string> findRepeatedDnaSequences(string s) {
         int n=s.length();
+        
         if(n<10)return {};
+        
         vector<string>ans;
-        unordered_map<string,int>mp;
-        int i=0,j=9;
-        string k=s.substr(0,10);
-        while(j<n){
-            if(mp.count(k)){
-                if(mp[k]==1)
-                    ans.push_back(k);
-                mp[k]++;
-            }else{
-                mp[k]++;
+        unordered_map<int,int>mp;
+        
+        for(int i=0;(i+10)<=n;i++){
+            int key=bit(s,i);
+            if(mp[key]==1){
+                ans.push_back(s.substr(i,10));
             }
-            k.erase(0,1);
-            if(j+1<n){
-                k+=s[j+1];
-            }
-            j++;
+            mp[key]++;
         }
+        
         return ans;
     }
 };
