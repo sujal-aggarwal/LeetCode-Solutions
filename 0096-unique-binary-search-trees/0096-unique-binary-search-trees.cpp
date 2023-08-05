c352f68a9fc025eb31b1cf7e0ccb16ae84c5d8b6
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-08-05 23:26:26 +0530

    Time: 6 ms (14.81%), Space: 6.4 MB (10.18%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    unordered_map<string,int>mp;
+    vector<vector<int>>dp;
     int solve(int start,int end){
         if(start>=end){
             return 1;
         }
         string key=to_string(start)+" "+to_string(end);
-        if(mp.count(key))
-            return mp[key];
+        if(dp[start][end]!=-1)
+            return dp[start][end];
         int ans=0;
         for(int i=start;i<=end;i++){
             int leftBst=solve(start,i-1);
             int rightBst=solve(i+1,end);
             ans+=(leftBst*rightBst);
         }
-        return mp[key]=ans;
+        return dp[start][end]=ans;
     }
     int numTrees(int n) {
+        dp.resize(n+1,vector<int>(n+1,-1));
         return solve(1,n);
     }
 };
