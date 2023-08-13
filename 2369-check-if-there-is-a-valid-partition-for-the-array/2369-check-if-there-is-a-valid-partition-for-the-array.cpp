6e891fdff888e91a4b0a5992fa06a761ffd3af67
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2023-08-14 01:15:24 +0530

    Time: 143 ms (74.92%), Space: 87.6 MB (79.20%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,30 +2,54 @@ class Solution {
 public:
     int n;
     vector<int>dp;
-    bool solve(vector<int>&nums,int index){
-        if(index>=n)return true;
-        if(index==n-1)return false;
-        if(index==n-2){
-            return nums[n-2]==nums[n-1];
+    bool solve(vector<int>&nums){
+        if(n==2){
+            return nums[0]==nums[1];
         }
-        if(dp[index]!=-1)return dp[index];
-        bool a=false;
-        if(nums[index]==nums[index+1]){
-            a=solve(nums,index+2);
+        dp[n]=1;
+        dp[n-1]=0;
+        dp[n-2]=(nums[n-2]==nums[n-1]);
+        for(int i=n-3;i>=0;i--){
+            bool a=false;
+            if(nums[i]==nums[i+1]){
+                a=dp[i+2];
+            }
+            bool b=false;
+            if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+                b=dp[i+3];
+            }
+            bool c=false;
+            if(nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]){
+                c=dp[i+3];
+            }
+            dp[i]=(a||b||c);
         }
-        bool b=false;
-        if(nums[index]==nums[index+1] && nums[index+1]==nums[index+2]){
-            b=solve(nums,index+3);
-        }
-        bool c=false;
-        if(nums[index]+1==nums[index+1] && nums[index+1]+1==nums[index+2]){
-            c=solve(nums,index+3);
-        }
-        return dp[index]=(a||b||c);
+        return dp[0];
     }
+    // bool solve(vector<int>&nums,int index){
+    //     if(index>=n)return true;
+    //     if(index==n-1)return false;
+    //     if(index==n-2){
+    //         return nums[n-2]==nums[n-1];
+    //     }
+    //     if(dp[index]!=-1)return dp[index];
+    //     bool a=false;
+    //     if(nums[index]==nums[index+1]){
+    //         a=solve(nums,index+2);
+    //     }
+    //     bool b=false;
+    //     if(nums[index]==nums[index+1] && nums[index+1]==nums[index+2]){
+    //         b=solve(nums,index+3);
+    //     }
+    //     bool c=false;
+    //     if(nums[index]+1==nums[index+1] && nums[index+1]+1==nums[index+2]){
+    //         c=solve(nums,index+3);
+    //     }
+    //     return dp[index]=(a||b||c);
+    // }
     bool validPartition(vector<int>& nums) {
         n=nums.size();
-        dp.resize(n,-1);
-        return solve(nums,0);
+        dp.resize(n+1,-1);
+        return solve(nums);
     }
 };
