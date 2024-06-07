e2b99a7143cac55160dc9fd955ab29296e66af82
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-06-07 12:46:21 +0530

    Time: 68 ms (85.93%), Space: 81.6 MB (40.23%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,32 +1,59 @@
+class TrieNode{
+    public:
+        bool isEnd;
+        vector<TrieNode*>children;
+        
+        TrieNode():isEnd(false),children(26,nullptr){
+        }
+};
+class Trie{
+    private:
+        TrieNode* root;
+    public:
+        Trie(){
+            root=new TrieNode();
+        }
+        
+        void insert(string word){
+            TrieNode* current= root;
+            for(char c:word){
+                if(current->children[c-'a']==nullptr){
+                    current->children[c-'a']=new TrieNode();
+                }
+                current = current->children[c-'a'];
+            }
+            current->isEnd=true;
+        }
+    
+        string shortestRoute(string word){
+            TrieNode* current=root;
+            for(int i=0;i<word.length();++i){
+                char c=word[i];
+                if(current->children[c-'a']==nullptr){
+                    return word;
+                }
+                current=current->children[c-'a'];
+                if(current->isEnd){
+                    return word.substr(0,i+1);
+                }
+            }
+            return word;
+        }
+};
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string,int>mp;
-        for(string s:dictionary){
-            mp[s]++;
+        stringstream sStream(sentence);
+        Trie dicTrie;
+        for(string word:dictionary){
+            dicTrie.insert(word);
         }
-        vector<string> ans;
-        stringstream str(sentence);
         string word;
-        while(str >> word){
-            ans.push_back(word);
+        string newSentence;
+        while(getline(sStream,word,' ')){
+            newSentence+=dicTrie.shortestRoute(word)+" ";
         }
-        for(string& s:ans){
-            string k="";
-            for(auto i:s){
-                k+=i;
-                if(mp.find(k)!=mp.end()){
-                    break;
-                }
-            }
-            s=k;
-        }
-        string s;
-        for(string i:ans){
-            s+=i;
-            s+=" ";
-        }
-        s.pop_back();
-        return s;
+        newSentence.pop_back();
+        return newSentence;
     }
 };
