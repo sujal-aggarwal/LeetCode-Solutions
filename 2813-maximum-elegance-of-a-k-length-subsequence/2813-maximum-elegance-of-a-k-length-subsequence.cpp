fbcb421f81ab2f512fac4ac844328e7093fcdbda
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-06-16 01:06:28 +0530

    Time: 485 ms (9.83%), Space: 216.2 MB (11.56%) - LeetHub

---
+++ 2813-maximum-elegance-of-a-k-length-subsequence/2813-maximum-elegance-of-a-k-length-subsequence.cpp
@@ -0,0 +1,43 @@
+#define ll long long
+class Solution {
+public:
+    long long findMaximumElegance(vector<vector<int>>& items, int k) {
+        int n=items.size();
+        unordered_map<ll,ll>mp;
+        ll ans=0;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        sort(items.begin(),items.end(),greater<vector<int>>());
+        for(int i=0;i<k;i++){
+            mp[items[i][1]]++;
+            ans+=items[i][0];
+            pq.push(items[i]);
+        }
+        
+        ll m=mp.size();
+        ans+=(m*m);
+        ll curr=ans;
+        for(int i=k;i<n;i++){
+            if(mp[items[i][1]]>=1)continue;
+            
+            while(!pq.empty() && mp[pq.top()[1]]==1)pq.pop();
+            
+            if(pq.empty())break;
+            
+            auto p=pq.top();
+            pq.pop();
+            curr-=p[0];
+            curr-=(m*m);
+            
+            curr+=items[i][0];
+            m++;
+            curr+=(m*m);
+            
+            mp[p[1]]--;
+            mp[items[i][1]]++;
+            pq.push(items[i]);
+            
+            ans=max(curr,ans);
+        }
+        return ans;
+    }
+};
