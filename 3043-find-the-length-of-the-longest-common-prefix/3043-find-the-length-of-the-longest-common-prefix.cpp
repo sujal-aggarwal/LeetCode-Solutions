09aa7e5e6eb237520f2a691780146574b1632d7d
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-06-17 16:04:26 +0530

    Time: 415 ms (46.10%), Space: 185.7 MB (29.55%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,24 +1,64 @@
+class TrieNode{
+    public:
+        vector<TrieNode*>next;
+        bool isEnd;
+
+        TrieNode():next(10,nullptr),isEnd{false}{}
+};
+class Trie{
+    private:
+        TrieNode* root;
+    public:
+        Trie(){
+            root=new TrieNode();
+        }
+
+        void insert(string s){
+            TrieNode* Node=root;
+            for(char c:s){
+                int i=c-'0';
+                if(Node->next[i]==nullptr){
+                    Node->next[i]=new TrieNode();
+                }
+                Node=Node->next[i];
+            }
+            Node->isEnd=true;
+        }
+    
+        int commonPrefix(string s){
+            TrieNode* Node=root;
+            int len=0;
+            for(char c:s){
+                int i=c-'0';
+                if(Node->next[i]==nullptr){
+                    return len;
+                }
+                Node=Node->next[i];
+                len++;
+            }
+            return len;
+        }
+};
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int,int>mp;
+        Trie trie;
         for(int i:arr1){
             int curr=i;
             while(curr){
-                mp[curr]++;
+                trie.insert(to_string(curr));
                 curr/=10;
             }
         }
-        int ans=0;
+        int maxlen=0;
         for(int i:arr2){
             int curr=i;
             while(curr){
-                if(mp.find(curr)!=mp.end()){
-                    ans=max(ans,(int)(log10(curr))+1);
-                }
+                int len=trie.commonPrefix(to_string(curr));
+                maxlen=max(maxlen,len);
                 curr/=10;
             }
         }
-        return ans;
+        return maxlen;
     }
 };
