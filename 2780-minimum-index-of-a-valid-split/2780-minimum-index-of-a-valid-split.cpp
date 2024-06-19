c056c3e62a3e2042216dcf27faeda5556b7ef5f8
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-06-20 00:29:10 +0530

    Time: 130 ms (70.40%), Space: 98.1 MB (52.02%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -8,20 +8,16 @@ public:
             mp[i]++;
             if(mp[i]>=((n/2)+1)){
                 target=i;
-                break;
             }
         }
-        vector<int>suffix(n,0);
-        suffix[n-1]=(nums[n-1]==target)?1:0;
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]+((nums[i]==target)?1:0);
-        }
+        int totalFreq=mp[target];
         int currFreq=0;
         for(int i=0;i<n-1;i++){
             if(nums[i]==target){
                 currFreq++;
             }
-            if(currFreq*2 > i+1 && suffix[i+1] * 2 > n-i-1  )return i;
+            int rightFreq=totalFreq-currFreq;
+            if(currFreq*2 > i+1 && rightFreq*2 > n-i-1)return i;
         }
         return -1;
     }
