d0a78cdc79a679d20b2c4970c458a62cdf84244f
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-07-04 23:56:56 +0530

    Time: 0 ms (100.00%), Space: 14.4 MB (87.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,22 +4,16 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return;
-        if(nums[n-1]>nums[n-2]){
-            swap(nums[n-1],nums[n-2]);
-            return ;
-        }
         int i=n-1;
         while(i>0){
             if(nums[i-1]>=nums[i])i--;
             else break;
         }
-        // cout<<"i "<<i<<" "<<nums[i]<<endl;
         if(i==0){
             sort(nums.begin(),nums.end());
             return;
         }
         int target = nums[i-1];
-        // cout<<"target "<<target<<endl;
         int l=i,r=n-1;
         int index=0;
         while(l<=r){
@@ -31,12 +25,7 @@ public:
                 index=mid;
             }
         }
-        // cout<<"index "<<index<<" "<<nums[index]<<endl;
         swap(nums[i-1],nums[index]);
-        // for(int i=0;i<n;i++){
-        //     cout<<nums[i]<<" ";
-        // }
-        // cout<<endl;
         sort(nums.begin()+i,nums.end());
     } 
 };
