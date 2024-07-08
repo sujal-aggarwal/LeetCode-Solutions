bfae271707f35395df12ecde6fcd3dd50c0bf48a
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-07-09 01:43:07 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (77.09%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    int solve(int n,int k){
-        if(n==1)return 0;
-        return (solve(n-1,k)+k)%n;
-    }
     int findTheWinner(int n, int k) {
-        return solve(n,k)+1;
+        int index=0;
+        for(int i=1;i<n;i++){
+            index+=k;
+            index%=(i+1);
+        }
+        return index+1;
     }
 };
