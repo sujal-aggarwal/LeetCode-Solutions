902ef388455cc81db64668c4aafe37f2e9b1789d
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-07-11 17:37:55 +0530

    Time: 3 ms (49.64%), Space: 7.9 MB (50.36%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
+        int n=s.size();
         string ans="";
-        vector<int>reverseIndex;
-        for(auto ch:s){
-            if(ch=='('){
-                reverseIndex.push_back(ans.size());
-            }else if(ch==')'){
-                reverse(ans.begin()+reverseIndex.back(),ans.end());
-                reverseIndex.pop_back();
+        stack<int>st;
+        vector<int>Pair(n,0);
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]==')'){
+                int index=st.top();
+                st.pop();
+                Pair[index]=i;
+                Pair[i]=index;
+            }
+        }
+        for(int currentIndex=0,direction=1;currentIndex<n;currentIndex+=direction){
+            if(s[currentIndex]=='(' || s[currentIndex]==')'){
+                currentIndex=Pair[currentIndex];
+                direction=-direction;
             }else{
-                ans+=ch;
+                ans+=s[currentIndex];
             }
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     string reverseParentheses(string s) {
+//         string ans="";
+//         vector<int>reverseIndex;
+//         for(auto ch:s){
+//             if(ch=='('){
+//                 reverseIndex.push_back(ans.size());
+//             }else if(ch==')'){
+//                 reverse(ans.begin()+reverseIndex.back(),ans.end());
+//                 reverseIndex.pop_back();
+//             }else{
+//                 ans+=ch;
+//             }
+//         }
+//         return ans;
+//     }
+// };
