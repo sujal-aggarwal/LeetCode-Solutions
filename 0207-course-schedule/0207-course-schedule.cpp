a0f9917ecbc4edbce89453977d669e5441112692
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-07-12 19:58:49 +0530

    Time: 14 ms (74.40%), Space: 18.6 MB (17.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     int n;
-    bool dfs(unordered_map<int,vector<int>>& adj,vector<int>& visited,vector<int>& inRecursion, int u){
-        visited[u]=1;
-        inRecursion[u]=1;
-        for(auto v:adj[u]){
-            if(visited[v] && inRecursion[v])return true;
-            if(!visited[v] && dfs(adj,visited,inRecursion,v))return true;
+    bool kahn(unordered_map<int,vector<int>>& adj){
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto j:adj[i]){
+                indegree[j]++;
+            }
         }
-        inRecursion[u]=0;
-        return false;
+        
+        int count=0;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+                count++;
+            }
+        }
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            
+            for(auto j:adj[node]){
+                indegree[j]--;
+                
+                if(indegree[j]==0){
+                    q.push(j);
+                    count++;
+                }
+            }
+        }
+        
+        return count==n;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         n=numCourses;
@@ -17,14 +40,6 @@ public:
         for(auto vec:prerequisites){
             adj[vec[0]].push_back(vec[1]);
         }
-        
-        vector<int>visited(n,0);
-        vector<int>inRecursion(n,0);
-        for(int i=0;i<n;i++){
-            if(!visited[i] && dfs(adj,visited,inRecursion,i)){
-                return false;
-            }
-        }
-        return true;
+        return kahn(adj);
     }
 };
