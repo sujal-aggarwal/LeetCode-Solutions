bf8fa8462ddf13aa60d02ee18d0c9681627edf94
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-07-15 22:50:48 +0530

    Time: 768 ms (32.53%), Space: 289.6 MB (42.80%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -11,38 +11,35 @@
  */
 class Solution {
 public:
-    TreeNode* solve(unordered_map<int,vector<int>>& mp,int root){
-        if(root==-1)return nullptr;
-        TreeNode* curr = new TreeNode(root);
-        if(mp.find(root)!=mp.end()){
-            curr->left=solve(mp,mp[root][0]);
-            curr->right=solve(mp,mp[root][1]);
-        }else{
-            curr->left=nullptr;
-            curr->right=nullptr;
-        }
-        return curr;
-    }
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int,vector<int>>mp;
+        unordered_map<int,TreeNode*> nodes;
         unordered_map<int,int>indegree;
-        for(auto des:descriptions){
-            if(mp.find(des[0])!=mp.end()){
-                mp[des[0]][1-des[2]]=des[1];
+        for(auto vec:descriptions){
+            int parent=vec[0];
+            int child=vec[1];
+            int isLeft=vec[2];
+            
+            indegree[parent];
+            indegree[child]++;
+            
+            TreeNode* ParentNode=(nodes.find(parent)!=nodes.end())?nodes[parent]:new TreeNode(parent);
+            TreeNode* childNode=(nodes.find(child)!=nodes.end())?nodes[child]:new TreeNode(child);
+            nodes[parent]=ParentNode;
+            nodes[child]=childNode;
+            
+            if(isLeft){
+                ParentNode->left=childNode;
             }else{
-                mp[des[0]]={-1,-1};
-                mp[des[0]][1-des[2]]=des[1];
-            }
-            indegree[des[1]]++;
-            indegree[des[0]];
-        }
-        int root=0;
-        for(auto [x,y]:indegree){
-            if(y==0){
-                root=x;
-                break;
+                ParentNode->right=childNode;
             }
         }
-        return solve(mp,root);
+        TreeNode* ans;
+        for(auto [i,j]:indegree){
+            if(j==0){
+                ans=nodes[i];
+            }
+        }
+        
+        return ans;
     }
 };
