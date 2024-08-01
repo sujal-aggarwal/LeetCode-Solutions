b8333a32e0cd6f5df577a36a3ac40f70c16132bc
Sujal Aggarwal <114332825+sujal-aggarwal@users.noreply.github.com>
2024-08-01 13:25:40 +0530

    Time: 6 ms (60.14%), Space: 17.7 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int cnt=0;
-        for(auto i:details){
-            cnt+=(stoi(i.substr(11,2))>60);
-        }
-        return cnt;
+        return accumulate(details.begin(),details.end(),0,[](int sum,string i){
+            return sum + (stoi(i.substr(11,2))>60); 
+        });
     }
 };
